e84273d72d34fbc1bbaca17e47717abf88d48992
Vyacheslav Shimarulin <shimarulin@gmail.com>
2019-02-10 14:44:20 +0300

    feat(eslint): add ivex eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,6 @@
 module.exports = {
   root: true,
   extends: [
-    '@platr/vue',
+    '@ivex/vue',
   ],
-  rules: {
-    'comma-dangle': [
-      'error',
-      {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'always-multiline',
-      },
-    ],
-    'vue/no-parsing-error': [
-      'error',
-      {
-        'x-invalid-end-tag': false
-      }
-    ],
-    'vue/component-name-in-template-casing': [
-      'error',
-      'PascalCase',
-      {
-        ignores: [],
-      },
-    ],
-  },
 }
